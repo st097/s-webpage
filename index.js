7cc992771414d0d11f63fdd185e53ffaafd6f08d
st097 <s.tozaj@gmail.com>
2023-09-03 12:37:48 +0200

    conected with firebase

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+var typed = new Typed( ".text", {
+    strings: ["HTML" , "CSS" , "JavaScript" , "React"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navBar = document.getElementsByClassName('navbar')[0]
 
@@ -6,11 +15,4 @@ toggleButton.addEventListener('click', () =>{
 })
 
 
-var typed = new Typed( ".text", {
-    strings: ["HTML" , "CSS" , "JavaScript" , "React"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-});
 
